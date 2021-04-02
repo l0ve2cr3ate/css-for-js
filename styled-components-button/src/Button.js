e1e313a4532e4330df7f3c56e5d7347df15c4152
l0ve2cr3ate <l0ve2cr3ate@gmail.com>
2021-04-02 21:30:29 +0200

    refactor: Create separate button variants using composition, for better readability.

--- styled-components-button/src/Button.js
+++ styled-components-button/src/Button.js
@@ -5,74 +5,67 @@ import { COLORS } from "./constants";
 
 const Btn = styled.button`
   text-transform: uppercase;
-  background-color: ${({ variant }) =>
-    variant === "fill"
-      ? COLORS.primary
-      : variant === "outline"
-      ? COLORS.white
-      : "transparent"};
+  border: 1px solid;
+  border-radius: 3px;
+  font-family: "Roboto", sans-serif;
   padding: ${({ size }) =>
     size === "large"
       ? "12px 24px"
       : size === "medium"
       ? "10px 20px"
       : "8px 16px"};
-  color: ${({ variant }) =>
-    variant === "fill"
-      ? COLORS.white
-      : variant === "outline"
-      ? COLORS.primary
-      : COLORS.transparentGray75};
-  border: 1px solid;
-  border-color: ${({ variant }) =>
-    variant === "fill"
-      ? "transparent"
-      : variant === "outline"
-      ? COLORS.primary
-      : "transparent"};
-  border-radius: 3px;
-  font-family: "Roboto", sans-serif;
   font-size: ${({ size }) =>
     size === "large" ? "1rem" : size === "medium" ? "0.875rem" : "0.75rem"};
-
   &:focus {
     outline-offset: 4px;
-    outline-color: ${({ variant }) =>
-      variant === "fill"
-        ? COLORS.primary
-        : variant === "outline"
-        ? COLORS.primary
-        : COLORS.transparentGray75};
   }
+`;
 
+const FillButton = styled(Btn)`
+  background-color: ${COLORS.primary};
+  color: ${COLORS.white};
+  border-color: transparent;
   &:hover {
-    background-color: ${({ variant }) =>
-      variant === "fill"
-        ? COLORS.primaryLight
-        : variant === "outline"
-        ? COLORS.offwhite
-        : COLORS.transparentGray15};
-    border-color: ${({ variant }) =>
-      variant === "fill"
-        ? "transparent"
-        : variant === "outline"
-        ? COLORS.primaryLight
-        : "transparent"};
-    color: ${({ variant }) =>
-      variant === "fill"
-        ? COLORS.white
-        : variant === "outline"
-        ? COLORS.primaryLight
-        : COLORS.black};
+    background-color: ${COLORS.primaryLight};
+    color: ${COLORS.white};
+  }
+  &:focus {
+    outline-color: ${COLORS.primary};
+  }
+`;
+
+const OutlineButton = styled(Btn)`
+  background-color: ${COLORS.white};
+  color: ${COLORS.primary};
+  border-color: ${COLORS.primary};
+  &:hover {
+    background-color: ${COLORS.offWhite};
+    border-color: ${COLORS.primaryLight};
+    color: ${COLORS.primaryLight};
+  }
+  &:focus {
+    outline-color: ${COLORS.primary};
+  }
+`;
+
+const GhostButton = styled(Btn)`
+  color: ${COLORS.transparentGray75};
+  border-color: transparent;
+  &:hover {
+    background-color: ${COLORS.transparentGray15};
+    color: ${COLORS.black};
+  }
+  &:focus {
+    outline-color: ${COLORS.transparentGray75};
   }
 `;
 
 const Button = ({ variant, size, children }) => {
-  return (
-    <Btn variant={variant} size={size}>
-      {children}
-    </Btn>
-  );
+  if (variant === "fill") {
+    return <FillButton size={size}>{children}</FillButton>;
+  } else if (variant === "outline") {
+    return <OutlineButton size={size}>{children}</OutlineButton>;
+  } else return <GhostButton size={size}>{children}</GhostButton>;
 };
 
 export default Button;
