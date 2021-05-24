6829289760451a690fbf430d770cda5da4024e6b
l0ve2cr3ate <l0ve2cr3ate@gmail.com>
2021-05-24 08:21:22 +0200

    Module 4 - Exercise 1 SuperHeader: Create Button to wrap around shopping-bag icon in SuperHeader.

---
+++ sole-and-ankle/src/components/UnstyledButton.js/UnstyledButton.js
@@ -0,0 +1,8 @@
+import styled from "styled-components/macro";
+const UnstyledButton = styled.button`
+  background: inherit;
+  border: none;
+  color: inherit;
+`;
+
+export default UnstyledButton;
