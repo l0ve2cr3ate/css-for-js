2c671a8ce9ff83be6b209723c7bca43c79abbfc4
l0ve2cr3ate <l0ve2cr3ate@gmail.com>
2021-06-02 10:01:49 +0200

    Module 4 Exercise 3 - Shell: Create Title component.

---
+++ sole-and-ankle/src/components/Title/Title.js
@@ -0,0 +1,9 @@
+import styled from "styled-components/macro";
+import { WEIGHTS } from "../../constants";
+
+const Title = styled.h2`
+  font-size: 1.5rem;
+  font-weight: ${WEIGHTS.medium};
+`;
+
+export default Title;
