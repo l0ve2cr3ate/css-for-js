84fe777c65ce57ea3c9c0527fca73545451f762e
l0ve2cr3ate <l0ve2cr3ate@gmail.com>
2021-06-28 09:46:01 +0200

    Module 5 - Sole and Ankle Revisited Workshop: Exercise 4 - Create Mobile Menu.

--- sole-and-ankle-revisited/src/components/MobileMenu/MobileMenu.js
+++ sole-and-ankle-revisited/src/components/MobileMenu/MobileMenu.js
@@ -1,13 +1,13 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
-import styled from 'styled-components/macro';
-import { DialogOverlay, DialogContent } from '@reach/dialog';
+import React from "react";
+import styled from "styled-components/macro";
+import { DialogOverlay, DialogContent } from "@reach/dialog";
 
-import { QUERIES } from '../../constants';
+import UnstyledButton from "../UnstyledButton";
+import Icon from "../Icon";
+import VisuallyHidden from "../VisuallyHidden";
+import { COLORS, WEIGHTS } from "../../constants";
 
-import UnstyledButton from '../UnstyledButton';
-import Icon from '../Icon';
-import VisuallyHidden from '../VisuallyHidden';
 
 const MobileMenu = ({ isOpen, onDismiss }) => {
   if (!isOpen) {
@@ -15,23 +15,91 @@ const MobileMenu = ({ isOpen, onDismiss }) => {
   }
 
   return (
-    <div>
-      <button onClick={onDismiss}>Dismiss menu</button>
-      <nav>
-        <a href="/sale">Sale</a>
-        <a href="/new">New&nbsp;Releases</a>
-        <a href="/men">Men</a>
-        <a href="/women">Women</a>
-        <a href="/kids">Kids</a>
-        <a href="/collections">Collections</a>
-      </nav>
-      <footer>
-        <a href="/terms">Terms and Conditions</a>
-        <a href="/privacy">Privacy Policy</a>
-        <a href="/contact">Contact Us</a>
-      </footer>
-    </div>
+    <StyledDialogOverlay isOpen={isOpen} onDismiss={onDismiss}>
+      <StyledDialogContent aria-label={"mobile-navigation opened"}>
+        <ButtonWrapper>
+          <UnstyledButton onClick={onDismiss}>
+            <VisuallyHidden>Close mobile navigation</VisuallyHidden>
+            <Icon color={COLORS.black} id="close" strokeWidth={2} />
+          </UnstyledButton>
+        </ButtonWrapper>
+        <MobileNav>
+          <NavLink className="active" href="/sale">
+            Sale
+          </NavLink>
+          <NavLink href="/new">New&nbsp;Releases</NavLink>
+          <NavLink href="/men">Men</NavLink>
+          <NavLink href="/women">Women</NavLink>
+          <NavLink href="/kids">Kids</NavLink>
+          <NavLink href="/collections">Collections</NavLink>
+        </MobileNav>
+        <Footer>
+          <NavLink href="/terms">Terms and Conditions</NavLink>
+          <NavLink href="/privacy">Privacy Policy</NavLink>
+          <NavLink href="/contact">Contact Us</NavLink>
+        </Footer>
+      </StyledDialogContent>
+    </StyledDialogOverlay>
   );
 };
 
 export default MobileMenu;
+
+const StyledDialogOverlay = styled(DialogOverlay)`
+  position: absolute;
+  background-color: hsl(220deg, 5%, 40%, 0.75);
+  width: 100vw;
+  min-height: 100vh;
+  height: 100%;
+  display: flex;
+  justify-content: flex-end;
+  top: 0;
+`;
+
+const StyledDialogContent = styled(DialogContent)`
+  position: absolute;
+  width: 90%;
+  height: 100%;
+  background-color: ${COLORS.white};
+  display: flex;
+  flex-direction: column;
+  padding-left: 48px;
+`;
+
+const ButtonWrapper = styled.div`
+  align-self: flex-end;
+  margin: 24px;
+`;
+
+const MobileNav = styled.nav`
+  display: flex;
+  flex-direction: column;
+  margin: auto 0;
+`;
+
+const Footer = styled.footer`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 24px;
+`;
+
+const NavLink = styled.a`
+  text-decoration: none;
+  padding: 7px 0;
+
+  ${MobileNav} & {
+    font-size: 22px;
+    color: ${COLORS.gray[900]};
+    font-weight: ${WEIGHTS.medium};
+    text-transform: uppercase;
+  }
+
+  ${Footer} & {
+    font-size: 16px;
+    color: ${COLORS.gray[700]};
+  }
+
+  &.active {
+    color: ${COLORS.secondary};
+  }
+`;
